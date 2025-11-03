8ccb3ad1b970521f75c812cbc5c3073f818bce57
SHREYA14019 <727824tuec229@skct.edu.in>
2025-11-03 21:02:51 +0530

    twosum.cpp

--- two sum.cpp
+++ two sum.cpp
@@ -1,1 +1,26 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n, t;
+    cin >> n;
+
+    vector<int> nums(n);
+    for (int i = 0; i < n; i++){
+        cin >> nums[i];}
+
+    cin >> t;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (nums[i] + nums[j] == t) {
+                cout << "[" << i << ", " << j << "]";
+                return 0;
+            }
+        }
+    }
+
+    return 0;
+}
+
